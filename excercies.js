8ab08057eb4f1ba86a2b71ec4687e1040814f6a9
ricardoggz <ricardo_ggzicm@outlook.com>
2021-11-27 23:48:14 -0600

    basic excercies

---
+++ excercies.js
@@ -0,0 +1,19 @@
+/*1. Escribir un programa que calcule el número de segundos que existen en un día.
+Para ello debemos multiplicar los segundos de un minuto, por los minutos de
+una hora, por las horas de un día. Como todo el mundo sabe, un día tiene 86400 segundos.*/
+
+const secondsDay = 60 * 60 * 24;
+console.log(secondsDay);
+
+/*2. Realiza un programa que a partir de los valores ancho=2 y alto=5,
+calcule el área de un rectángulo.*/
+
+const area = 2 * 5;
+console.log(area);
+
+/*3. Hacer un conversor de grados centígrados a grados Fahrenheit.
+Para ello deberé multiplicar por 9/5 y sumar 32.
+Como todo el mundo sabe, 20 grados centígrados son 68 grados Fahrenheit.*/
+
+const converter = 20 * 1.8 + 32;
+console.log(converter);
