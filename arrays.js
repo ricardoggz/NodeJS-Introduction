17eda1742ac6cefb280e26b5f563bdc6ef0a3f2b
ricardoggz <ricardo_ggzicm@outlook.com>
2021-12-01 18:59:53 -0600

    leccion: manipulacion de arays con filter, agregada

--- arrays.js
+++ arrays.js
@@ -11,3 +11,25 @@ console.log(posicion1, posicion2);
 //operador de miembro o acceso de propiedad:
 let arrayFruits = ["apple", "banana", "lemon"]; //= 0,1,2
 console.log(arrayFruits[0], arrayFruits[1]);
+
+//manipulación de arreglos con filter:
+//filter no modifica el arreglo, filter arrojará un nuevo arreglo
+
+let students = [
+  { name: "Ricardo", age: 21, size: 1.71 },
+  { name: "Fernando", age: 23, size: 1.76 },
+  { name: "Ernesto", age: 15, size: 1.71 },
+];
+
+const filtrado = students.filter(student => student.age > 20 && student.size > 1.71); 
+console.log(filtrado);
+
+//
+const ovejas = [
+  { name: "Noa", color: "azul" },
+  { name: "Euge", color: "rojo" },
+  { name: "Navidad", color: "rojo" },
+  { name: "Ki Na Ma", color: "rojo" },
+];
+const filtrarOvejas = ovejas.filter(oveja => oveja.color === "rojo");
+console.log(filtrarOvejas);
