2f787b7504cd0352896ac4d881d8df472d26dc7a
ricardoggz <ricardo_ggzicm@outlook.com>
2021-12-01 19:47:56 -0600

    leccion ciclos: while y do while

---
+++ ciclos.js
@@ -0,0 +1,23 @@
+{
+  //ciclos while:
+  //primero evalua la condición y después ejecuta un ciclo
+
+  let contador = 0;
+  while (contador < 5) {
+    contador = contador += 1;
+    console.log("Contador");
+  }
+}
+
+
+
+{
+  //ciclos do while
+  //se ejecuta al menos una vez y después evalúa la condición
+
+  let contador = 0;
+  do {
+    contador = contador += 1;
+    console.log("contador");
+  } while (contador <= 10);
+}
