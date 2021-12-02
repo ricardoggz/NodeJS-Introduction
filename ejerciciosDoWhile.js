cda54870112b35f8ba4cc99dfca46ce784b9b809
ricardoggz <ricardo_ggzicm@outlook.com>
2021-12-02 11:49:37 -0600

    leccion ciclos: ejercicios con do while

---
+++ ejerciciosDoWhile.js
@@ -0,0 +1,30 @@
+//ejercicios con ciclos:
+
+/*1.Imprima las tablas de multiplicar del 2 al 12 utilizando la declaración do while*/
+
+{
+  //tabla del 2:
+  let i = 0;
+  do {
+    i = i += 2;
+    console.log(i);
+  } while (i < 20);
+}
+
+{
+  //tabla del 3:
+  let i = 0;
+  do {
+    i = i += 3;
+    console.log(i);
+  } while (i < 30);
+}
+
+{
+  //tabla del 3:
+  let i = 0;
+  do {
+    i = i += 4;
+    console.log(i);
+  } while (i < 40);
+}
