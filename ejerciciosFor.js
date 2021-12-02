7cb9c6c9b43c5f04b458609862fe8fa2dabe2ff4
ricardoggz <ricardo_ggzicm@outlook.com>
2021-12-01 23:43:17 -0600

    ejercicios de ciclos

---
+++ ejerciciosFor.js
@@ -0,0 +1,102 @@
+//ejercicios con ciclos:
+
+/*1.Imprima las tablas de multiplicar del 2 al 12 utilizando la declaración for*/
+
+{
+  //tabla del 2
+  let numero = 2;
+  for (let i = 1; i <= 10; i++) {
+    let multiplicacion = numero * i;
+    console.log(`${numero} * ${i} = ${multiplicacion}`);
+  }
+}
+
+{
+  //tabla del 3
+  let numero = 3;
+  for (let i = 1; i <= 10; i++) {
+    let multiplicacion = numero * i;
+    console.log(`${numero} * ${i} = ${multiplicacion}`);
+  }
+}
+
+{
+  //tabla del 4
+  let numero = 4;
+  for (let i = 1; i <= 10; i++) {
+    let multiplicacion = numero * i;
+    console.log(`${numero} * ${i} = ${multiplicacion}`);
+  }
+}
+
+{
+  //tabla del 5
+  let numero = 5;
+  for (let i = 1; i <= 10; i++) {
+    let multiplicacion = numero * i;
+    console.log(`${numero} * ${i} = ${multiplicacion}`);
+  }
+}
+
+{
+  //tabla del 6
+  let numero = 6;
+  for (let i = 1; i <= 10; i++) {
+    let multiplicacion = numero * i;
+    console.log(`${numero} * ${i} = ${multiplicacion}`);
+  }
+}
+
+{
+  //tabla del 7
+  let numero = 7;
+  for (let i = 1; i <= 10; i++) {
+    let multiplicacion = numero * i;
+    console.log(`${numero} * ${i} = ${multiplicacion}`);
+  }
+}
+
+{
+  //tabla del 8
+  let numero = 8;
+  for (let i = 1; i <= 10; i++) {
+    let multiplicacion = numero * i;
+    console.log(`${numero} * ${i} = ${multiplicacion}`);
+  }
+}
+
+{
+  //tabla del 9
+  let numero = 9;
+  for (let i = 1; i <= 10; i++) {
+    let multiplicacion = numero * i;
+    console.log(`${numero} * ${i} = ${multiplicacion}`);
+  }
+}
+
+{
+  //tabla del 10
+  let numero = 10;
+  for (let i = 1; i <= 10; i++) {
+    let multiplicacion = numero * i;
+    console.log(`${numero} * ${i} = ${multiplicacion}`);
+  }
+}
+
+{
+  //tabla del 11
+  let numero = 11;
+  for (let i = 1; i <= 10; i++) {
+    let multiplicacion = numero * i;
+    console.log(`${numero} * ${i} = ${multiplicacion}`);
+  }
+}
+
+{
+  //tabla del 12
+  let numero = 12;
+  for (let i = 1; i <= 10; i++) {
+    let multiplicacion = numero * i;
+    console.log(`${numero} * ${i} = ${multiplicacion}`);
+  }
+}
---
+++ ejerciciosWhile.js
@@ -0,0 +1,32 @@
+//ejercicios con ciclos:
+
+/*1.Imprima las tablas de multiplicar del 2 al 12 utilizando la declaración while*/
+
+{
+  //tabla del 2
+  let i = 0;
+  while (i < 20) {
+    i = i += 2;
+    console.log(i);
+  }
+}
+
+{
+  //tabla del 3
+  let i = 0;
+  while (i < 30) {
+    i = i += 3;
+    console.log(i);
+  }
+
+}
+
+{
+    //tabla del 4
+    let i = 0;
+    while (i < 40) {
+      i = i += 4;
+      console.log(i);
+    }
+  
+  }
