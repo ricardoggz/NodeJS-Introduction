f9168a4f310ba5209fae04eefda4d58583e17ace
ricardoggz <ricardo_ggzicm@outlook.com>
2021-12-02 18:21:19 -0600

    leccion functions: agregada

--- functions.js
+++ functions.js
@@ -6,3 +6,47 @@ function saludar() {
   console.log("Hola");
 }
 saludar();
+
+{
+  //una funcion puede recibir parámetros :
+  function saludo(nombre /*parámetro*/) {
+    console.log(`Hola ${nombre}`);
+  }
+  saludo("José");
+}
+
+{
+  //otra manera de declarar cuntiones es retornando valores:
+  function saludar(nombre) {
+    return `Hola ${nombre}`;
+  }
+  let saludo = saludar("Ricardo");
+  console.log(saludo);
+}
+
+{
+  //también las funciones pueden almacenarse en variables:
+  let miFuncion = function (a, b) {
+    return a + b;
+  };
+
+  let resultado = miFuncion(2, 2);
+  console.log(resultado);
+}
+
+//arrow functions:
+//son alternativas para compactar a una expresion de funcion
+//son más limitadas
+{
+  const restar = (a, b) => {
+    return a - b;
+  };
+  console.log(restar(2, 1));
+}
+
+{
+  //las arrow function tienen un return implícito:
+  //reduce el codigo si es que solo se requiere una línea
+  const multiplicar = (a, b) => a * b;
+  console.log(multiplicar(2, 2));
+}
