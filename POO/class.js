bb14caf2461de7c1ef8ab7cafdd2bb8cfc1c6b09
ricardoggz <ricardo_ggzicm@outlook.com>
2022-02-11 09:15:06 -0600

    complementacion de leccion de clases

--- POO/class.js
+++ POO/class.js
@@ -4,12 +4,13 @@
 //Las clase se identifican por que contienen propiedades y métodos
 
 class User {
-  constructor(nombre, apellido) {
+  constructor(nombre, apellido, user) {
     this.nombre = nombre;
     this.apellido = apellido;
+    this.user= user;
   }
 }
 
 //así llamaremos a la clase y le pondremos valores al constructor
-const user= new User("Ricardo", "Guevara");
+const user= new User("Ricardo", "Guevara", "@ricardo_guevarag");
 console.log(user);
