e32e435553dcc9f771d1e45e9986d7fa49fee81d
ricardoggz <ricardo_ggzicm@outlook.com>
2022-05-09 11:07:50 -0500

    complementacion de this en javascript

--- Functions/objectThis.js
+++ Functions/objectThis.js
@@ -5,3 +5,31 @@
 
 const isWindow = () => this === global;
 console.log(isWindow());
+
+//en el caso de un método el objeto this sería el valor del ámbito de la función. por ejemplo:
+
+{
+    const counter = {
+        number: 0,
+        increment() {
+            setInterval(() => console.log(++this.number), 1000);
+        }
+    }
+    console.log(counter.increment()); //output: 1, 2, 3 ...
+}
+
+//en este otro caso el objeto this pierde la referencia del objeto counter, ya que no estamos utilizando arrow functions
+
+{
+    const counter = {
+        number: 0,
+        increment() {
+            setInterval(function(){
+                console.log(++this.number);
+            }, 1000);
+        }
+    }
+
+    let { increment } = counter;
+    console.log(increment()); //output: NaN, NaN ...
+}
