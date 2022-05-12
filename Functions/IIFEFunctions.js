34f522aa93388ce91b6a015170f362f3d75e9ecb
ricardoggz <ricardo_ggzicm@outlook.com>
2022-05-11 20:36:42 -0500

    leccion de iife functions agregada

---
+++ Functions/IIFEFunctions.js
@@ -0,0 +1,7 @@
+/* las funciones IIFE o funciones autoejecutadas, son aquellas funciones que se ejecutan inmediatamente después de definirlas:*/
+
+(function IIFE() {
+  return console.log("IIFE function");
+})();
+
+/* Este patrón se utilizaba antes de ES6 para crear un ámbito de bloque, es decir antes de la llegada de let y const */
